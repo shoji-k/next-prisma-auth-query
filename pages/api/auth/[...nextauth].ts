9aa3c2ca942bebd7c94fafe96b0d44e3c003d64e
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2021-06-01 21:30:16 +0900

    GET todos api

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,6 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
-import Adapters from "next-auth/adapters";
-import { PrismaClient } from "@prisma/client";
 
-let prisma;
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
 const options = {
   providers: [
     Providers.Google({
@@ -19,6 +8,11 @@ const options = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  adapter: Adapters.Prisma.Adapter({ prisma }),
+  callbacks: {
+    session: async (session, user) => {
+      session.user.id = user.id;
+      return Promise.resolve(session);
+    },
+  },
 };
 export default (req, res) => NextAuth(req, res, options);
---
+++ pages/api/todos.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+import { getSession } from "next-auth/client";
+
+const prisma = new PrismaClient();
+  
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getSession({ req });
+  if (!session) return res.status(401).end("Please log in to view");
+
+  const userId = session.user.id;
+
+  if (req.method === "GET") {
+    const todos = await prisma.todo.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      where: {
+        userId,
+      },
+    });
+    return res.status(200).json(todos);
+  }
+};
